6af777da697e6ca1abd1f870ed133fb11f8f6a49
Johan Quiroga <quirogacj@utp.edu.co>
2018-04-07 16:30:30 -0500

    Fixing add ToDo action so it's added on the server and updated on the state

--- src/api.js
+++ src/api.js
@@ -26,7 +26,7 @@ const delay = (ms) =>
 export const fetchTodos = (filter) =>
   delay(500).then(() => {
     if (Math.random() > 0.5) {
-      throw new Error('Boom!');  
+      throw new Error('Boom!');
     }
 
     switch (filter) {
@@ -39,4 +39,22 @@ export const fetchTodos = (filter) =>
       default:
         throw new Error(`Unknown filter: ${filter}`);
     }
+  });
+
+export const addTodo = (text) =>
+  delay(500).then(() => {
+    const todo = {
+      id: v4(),
+      text,
+      completed: false,
+    };
+    fakeDatabase.todos.push(todo);
+    return todo;
+  });
+
+export const toggleTodo = (id) =>
+  delay(500).then(() => {
+    const todo = fakeDatabase.todos.find(t => t.id === id);
+    todo.completed = !todo.completed;
+    return todo;
   });
