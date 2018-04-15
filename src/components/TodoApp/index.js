ddca4a97bdec15f4beb266d5990fcccbfb68e024
Johan Quiroga <quirogacj@utp.edu.co>
2018-04-15 16:24:43 -0500

    Added styles for general app

--- src/components/TodoApp/index.js
+++ src/components/TodoApp/index.js
@@ -1,13 +1,27 @@
 import React from 'react';
+import { Grid, Container, Segment, Header } from 'semantic-ui-react';
 import AddTodo from '../AddTodo';
 import VisibleTodoList from '../VisibleTodoList';
 import Footer from '../Footer';
 
 const TodoApp = () => (
   <div>
-    <AddTodo />
-    <VisibleTodoList />
-    <Footer />
+    <Container text>
+      <Grid
+        textAlign='center'
+        verticalAlign='middle'
+        padded
+      >
+        <Grid.Column>
+          <Header as='h1' color='teal'>
+            Todos
+          </Header>
+          <AddTodo />
+          <VisibleTodoList />
+          <Footer />
+        </Grid.Column>
+      </Grid>
+    </Container>
   </div>
 );
 
