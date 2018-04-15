723b80440c72f28789b0432afc2be6301d28db2f
Johan Quiroga <quirogacj@utp.edu.co>
2018-04-15 16:25:32 -0500

    Added styling for adding todo

--- src/components/AddTodo/index.js
+++ src/components/AddTodo/index.js
@@ -1,24 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Form, Segment } from 'semantic-ui-react';
 import { addTodo } from '../../store/actions';
 
-let AddTodo = ({ dispatch }) => {
-  let input;
+class AddTodo extends Component {
+  constructor(props) {
+    super(props);
 
-  return (
-    <div>
-      <input ref={node => {
-        input = node;
-      }} />
-      <button onClick={() => {
-        dispatch(addTodo(input.value));
-        input.value = '';
-      }}>
-        Add Todo
-      </button>
-    </div>
-  );
-};
+    this.state = {
+      input: ''
+    };
+  }
+
+  render() {
+    const { dispatch } = this.props;
+    const { input } = this.state;
+
+    return (
+        <Form
+          size='large'
+          onSubmit={() => {
+            if (input.trim() !== '') {
+              dispatch(addTodo(input.trim()));
+              this.setState({ input: '' });
+            }
+          }}
+        >
+          <Segment basic>
+            <Form.Input
+              name='input'
+              value={input}
+              placeholder='What do you need?'
+              fluid
+              onChange={(e, {name, value}) => this.setState({ [name]: value })}
+            />
+          </Segment>
+        </Form>
+    );
+  }
+}
 AddTodo = connect()(AddTodo);
 
 export default AddTodo;
