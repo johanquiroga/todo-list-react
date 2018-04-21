8586e8aa7c48e8d69389df9e5f2a278aa7043b74
Johan Quiroga <quirogacj@utp.edu.co>
2018-04-20 20:26:31 -0500

    Refactored AddTodo component to make it stateless again

--- src/components/AddTodo/index.js
+++ src/components/AddTodo/index.js
@@ -1,44 +1,33 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
+import { withState, compose } from 'recompose';
 import { Form, Segment } from 'semantic-ui-react';
 import { addTodo } from '../../store/actions';
 
-class AddTodo extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      input: ''
-    };
-  }
-
-  render() {
-    const { dispatch } = this.props;
-    const { input } = this.state;
-
-    return (
-        <Form
-          size='large'
-          onSubmit={() => {
-            if (input.trim() !== '') {
-              dispatch(addTodo(input.trim()));
-              this.setState({ input: '' });
-            }
-          }}
-        >
-          <Segment basic>
-            <Form.Input
-              name='input'
-              value={input}
-              placeholder='What do you need?'
-              fluid
-              onChange={(e, {name, value}) => this.setState({ [name]: value })}
-            />
-          </Segment>
-        </Form>
-    );
-  }
-}
-AddTodo = connect()(AddTodo);
+let AddTodo = ({dispatch, input, setInput}) => (
+    <Form
+      size='large'
+      onSubmit={() => {
+        if (input.trim() !== '') {
+          dispatch(addTodo(input.trim()));
+          setInput('');
+        }
+      }}
+    >
+      <Segment basic>
+        <Form.Input
+          name='input'
+          value={input}
+          placeholder='What do you need?'
+          fluid
+          onChange={(e, {name, value}) => setInput(value)}
+        />
+      </Segment>
+    </Form>
+);
+AddTodo = compose(
+  connect(),
+  withState('input', 'setInput', ''),
+)(AddTodo);
 
 export default AddTodo;
