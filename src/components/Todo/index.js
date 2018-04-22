3401f55f740206b712977d13699d614fdb9a3ca7
Johan Quiroga <quirogacj@utp.edu.co>
2018-04-22 15:26:06 -0500

    Added events so delete todo option only appears on focused todo

--- src/components/Todo/index.js
+++ src/components/Todo/index.js
@@ -1,25 +1,34 @@
 import React from 'react';
 import { List } from 'semantic-ui-react';
+import { withState, compose } from 'recompose';
 
 const Todo = ({
   onTodoClick,
   onDeleteClick,
   completed,
-  text
+  text,
+  isFocused,
+  setFocus
 }) => (
     <List.Item
-      onDoubleClick={() => console.log('double click')} active={!completed}
+      onDoubleClick={() => console.log('double click')}
+      active={!completed}
+      onMouseEnter={() => setFocus(true)}
+      onMouseLeave={() => setFocus(false)}
     >
-      <List.Content floated='right'>
-        <List.Icon
-          verticalAlign='middle'
-          onClick={onDeleteClick}
-          size='large'
-          name='close'
-          link
-          color='red'
-        />
-      </List.Content>
+      {
+        isFocused &&
+        <List.Content floated='right'>
+          <List.Icon
+            verticalAlign='middle'
+            onClick={onDeleteClick}
+            size='large'
+            name='close'
+            link
+            color='red'
+          />
+        </List.Content>
+      }
       <List.Icon
         verticalAlign='middle'
         onClick={onTodoClick}
@@ -41,4 +50,6 @@ const Todo = ({
     </List.Item>
 );
 
-export default Todo;
+export default compose(
+  withState('isFocused', 'setFocus', false),
+)(Todo);
